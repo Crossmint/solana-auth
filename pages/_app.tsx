450fcd34c4f1518c6963b14479d363d40b9c2ac3
bjar-paella <ben@paella.dev>
2021-12-17 08:41:32 -0500

    copying next-starter

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,30 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import { AppProps } from 'next/app';
+import dynamic from 'next/dynamic';
+import { FC, ReactNode } from 'react';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+// Use require instead of import, and order matters
+require('../styles/globals.css');
+require('@solana/wallet-adapter-react-ui/styles.css');
 
-export default MyApp
+const WalletConnectionProvider = dynamic<{ children: ReactNode }>(
+    () =>
+        import('../components/WalletConnectionProvider').then(
+            ({ WalletConnectionProvider }) => WalletConnectionProvider
+        ),
+    {
+        ssr: false,
+    }
+);
+
+const App: FC<AppProps> = ({ Component, pageProps }) => {
+    return (
+        <WalletConnectionProvider>
+            <WalletModalProvider>
+                <Component {...pageProps} />
+            </WalletModalProvider>
+        </WalletConnectionProvider>
+    );
+};
+
+export default App;
