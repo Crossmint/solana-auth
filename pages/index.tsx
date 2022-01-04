d827af188b5cb80ce48bf6dcad57a61d93528d97
bjar-paella <ben@paella.dev>
2022-01-04 09:54:13 -0500

    using react hooks for fb signin

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,12 @@ import { NextPage } from "next";
 import Head from "next/head";
 import SolanaFirebaseAuth from "../components/SolanaFirebaseAuth";
 import styles from "../styles/Home.module.css";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../utils/firebaseClient";
 
 const Index: NextPage = () => {
+  const [user, loading, error] = useAuthState(auth);
+
   return (
     <div className={styles.container}>
       <Head>
