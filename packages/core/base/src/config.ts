58e9fef93876b75db58d5f653c944994b8e964a3
bjar-paella <ben@paella.dev>
2022-01-15 12:49:48 -0500

    Using env for auth domain

--- packages/core/base/src/config.ts
+++ packages/core/base/src/config.ts
@@ -1,4 +1,4 @@
-export const AUTH_DOMAIN = "desolate.space";
+export const AUTH_DOMAIN = process.env.SOLANA_AUTH_DOMAIN || "";
 
 const nonceStr = (nonce: string) => `|| id=${nonce}`;
 export const signInMessage = (nonce: string, domain: string) =>
