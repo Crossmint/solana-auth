17c19c6d1c8ed31d2a6769599d5f80f812bc3b6e
bjar-paella <ben@paella.dev>
2022-01-20 08:05:55 -0500

    changing names

--- packages/core/base/src/config.ts
+++ packages/core/base/src/config.ts
@@ -1,4 +1,4 @@
-export const AUTH_DOMAIN = process.env.SOLANA_AUTH_DOMAIN || "";
+export const AUTH_DOMAIN = process.env.AUTH_DOMAIN || "";
 
 const nonceStr = (nonce: string) => `|| id=${nonce}`;
 export const signInMessage = (nonce: string, domain: string) =>
