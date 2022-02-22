528873676b21a78b2cceeb139fb4d4ff7c9666db
bjar-paella <ben@paella.dev>
2022-02-21 21:41:24 -0500

    fix: updating the example directory

--- packages/examples/nextjs-starter/next.config.js
+++ packages/examples/nextjs-starter/next.config.js
@@ -1,5 +1,9 @@
 // /* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('next').NextConfig} */
-module.exports = {
+const withTM = require("next-transpile-modules")([
+  "@crossmint/solana-auth-react-ui",
+]); // pass the modules you would like to see transpiled
+
+module.exports = withTM({
   reactStrictMode: true,
-};
+});
