1a09a79cc6605ef9e8f297119fb14df5ace6c877
bjar-paella <ben@paella.dev>
2022-03-02 14:24:36 -0500

    upgrading next

--- packages/examples/nextjs-starter/next.config.js
+++ packages/examples/nextjs-starter/next.config.js
@@ -1,9 +1,5 @@
 // /* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('next').NextConfig} */
-const withTM = require("next-transpile-modules")([
-  "@crossmint/solana-auth-react-ui",
-]); // pass the modules you would like to see transpiled
-
-module.exports = withTM({
+module.exports = {
   reactStrictMode: true,
-});
+};
